141999a39384950223bfc80c89bdd36b71673c4f
GouravJounwal <76249754+GouravJounwal@users.noreply.github.com>
2022-03-04 12:34:49 +0530

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,10 @@
+a = 1
+print(a, "is of type", type(a))
+
+a = 7.0
+print(a, "is of type", type(a))
+
+a = 1+2j
+print(a, "is complex number?", isinstance(1+2j,complex))
+a = [1, 2.2, 'gourav']
+print(a)
