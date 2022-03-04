b1485129f49231dc6f3ebac7d8e45fc754dcfeaa
GouravJounwal <76249754+GouravJounwal@users.noreply.github.com>
2022-03-04 12:47:54 +0530

    Create 2nd_max_of_3_no.py

---
+++ 2nd_max_of_3_no.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if (a <= b and a >= c) or (a <= c and a >= b):
+        print(a)
+    elif (b <= a and b >= c) or (b <= c and b >= a):
+        print(b)
+    elif (c <= a and c >= b) or (c <= b and c >= a):
+        print(c)
