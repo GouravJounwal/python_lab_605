a63fc13c2edc35889a383aef2286f3c625e4e4fc
GouravJounwal <76249754+GouravJounwal@users.noreply.github.com>
2022-03-04 12:38:00 +0530

    Create add_two_no.py

---
+++ add_two_no.py
@@ -0,0 +1,10 @@
+a=1
+b=2
+print(a+b)
+#by input
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+sum = float(num1) + float(num2)
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
